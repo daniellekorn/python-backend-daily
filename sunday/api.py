da5509c5a35f085b2445c34c101bbdfb09352ecf
Danielle Korn <daniella.korn@gmail.com>
2020-05-17 18:48:01 +0300

    draft for comments class and global com. dict

--- sunday/api.py
+++ sunday/api.py
@@ -1,5 +1,6 @@
-from flask import Flask, json
+from flask import Flask
 from sunday.classes.jsonablePost import JsonablePost
+from sunday.classes.comment import Comment
 from datetime import datetime, timedelta
 import requests
 import random
@@ -11,13 +12,28 @@ global_posts_dict = {}
 post_comments = {}
 
 
-@app.before_first_request
+# @app.before_first_request
 def get_all_posts():
     get_posts = requests.get('https://jsonplaceholder.typicode.com/posts')
     response_data = get_posts.json()
     for post in response_data:
         current_post = JsonablePost(post, created_at())
         global_posts_dict[post['id']] = current_post.to_json()
+    return global_posts_dict
+
+
+@app.before_first_request
+def get_comments():
+    for n in range(1, 10):
+        comments = requests.get('https://jsonplaceholder.typicode.com/posts/' + str(n) + '/comments')
+        response_data = comments.json()
+        for i in response_data:
+            current_comment = Comment(i['postId'], i['id'], i['name'], i['email'], i['body'])
+            post_comments[i['id']] = current_comment
+
+
+def comments_up_to_post_ten():
+    get_posts = requests.get('https://jsonplaceholder.typicode.com/posts')
 
 
 # function to generate random recent date for realistic post creation time
--- sunday/classes/comment.py
+++ sunday/classes/comment.py
@@ -1,5 +1,4 @@
-class Comment(__dict__):
+class Comment(dict):
 
-    def __init__(self):
-        dict.__init__(self)
-
+    def __init__(self, post_id, comment_id, name, email, body):
+        dict.__init__(self, postId=post_id, id=comment_id, name=name, email=email, body=body)
