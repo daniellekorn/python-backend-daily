514e08dae993ab14a84c5cf7a767ecb5ed583dbc
Danielle Korn <daniella.korn@gmail.com>
2020-05-17 15:56:13 +0300

    function to create rand date

--- sunday/api.py
+++ sunday/api.py
@@ -1,12 +1,13 @@
 from flask import Flask, json
 from sunday.classes.jsonablePost import JsonablePost
-from datetime import datetime
+from datetime import datetime, timedelta
 import requests
+import random
 
 app = Flask(__name__)
 
 
-global_post_dict = {}
+global_posts_dict = {}
 
 
 @app.before_first_request
@@ -14,11 +15,18 @@ def get_all_posts():
     get_posts = requests.get('https://jsonplaceholder.typicode.com/posts')
     response_data = get_posts.json()
     for i in response_data:
-        current_post = JsonablePost(i['userId'], i['id'], i['title'], i['body'], datetime.now())
+        current_post = JsonablePost(i['userId'], i['id'], i['title'], i['body'], random_date())
         post_instance = ({i['id']: current_post.return_as_obj()})
-        global_post_dict.update(post_instance)
+        global_posts_dict.update(post_instance)
+    print(global_posts_dict)
 
-    print(global_post_dict)
+
+def random_date():
+    end = datetime.now()
+    start = end - timedelta(weeks=30)
+    date = start + (end - start) * random.random()
+    str_date = date.strftime('%m/%d/%Y')
+    return str_date
 
 
 if __name__ == "__main__":
---
+++ sunday/classes/comment.py
@@ -0,0 +1,5 @@
+class Comment(__dict__):
+
+    def __init__(self):
+        dict.__init__(self)
+
