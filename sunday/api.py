ab7a912c622dfb79889de4175eaa0e127e377bf5
Danielle Korn <daniella.korn@gmail.com>
2020-05-17 13:44:57 +0300

    exercise 1 complete

--- sunday/api.py
+++ sunday/api.py
@@ -4,36 +4,32 @@ import requests
 app = Flask(__name__)
 
 
+# generic reusable function for translating get request to json
+def get_json(url):
+    request = requests.get(url)
+    response_data = request.json()
+    response = app.response_class(response=json.dumps(response_data), status=200, mimetype='application/json')
+    return response
+
+
 @app.route("/posts")
 def get_posts():
-    posts_response = requests.get('https://jsonplaceholder.typicode.com/posts')
-    posts = posts_response.json()
-    response = app.response_class(response=json.dumps(posts), status=200, mimetype='application/json')
-    return response
+    return get_json('https://jsonplaceholder.typicode.com/posts')
 
 
 @app.route("/posts/<post_id>")
 def get_post_by_id(post_id):
-    post_response = requests.get('https://jsonplaceholder.typicode.com/posts/' + post_id)
-    post = post_response.json()
-    response = app.response_class(response=json.dumps(post), status=200, mimetype='application/json')
-    return response
-
-
-@app.route("/users/<user_id>")
-def get_users_posts(user_id):
-    user_response = requests.get('https://jsonplaceholder.typicode.com/users/' + user_id)
-    user_posts = user_response.json()
-    response = app.response_class(response=json.dumps(user_posts), status=200, mimetype='application/json')
-    return response
+    return get_json('https://jsonplaceholder.typicode.com/posts/' + post_id)
 
 
 @app.route("/posts/<post_id>/comments")
 def get_post_comments(post_id):
-    posts_response = requests.get('https://jsonplaceholder.typicode.com/posts/' + post_id + '/comments')
-    posts = posts_response.json()
-    response = app.response_class(response=json.dumps(posts), status=200, mimetype='application/json')
-    return response
+    return get_json('https://jsonplaceholder.typicode.com/posts/' + post_id + '/comments')
+
+
+@app.route("/users/<user_id>")
+def get_single_users_posts(user_id):
+    return get_json('https://jsonplaceholder.typicode.com/users/' + user_id)
 
 
 if __name__ == "__main__":
