dfbdf4991a07ba82902cea0670e826be1323d7aa
Danielle Korn <daniella.korn@gmail.com>
2020-05-17 15:24:45 +0300

    create sub dict with key

--- sunday/api.py
+++ sunday/api.py
@@ -6,13 +6,19 @@ import requests
 app = Flask(__name__)
 
 
+global_post_dict = {}
+
+
 @app.before_first_request
 def get_all_posts():
     get_posts = requests.get('https://jsonplaceholder.typicode.com/posts')
     response_data = get_posts.json()
     for i in response_data:
         new_item = JsonablePost(i['userId'], i['id'], i['title'], i['body'], datetime.now())
-        print(new_item)
+        post_dict_with_id_key = ({i['id']: new_item})
+        global_post_dict.update(post_dict_with_id_key)
+
+    print(global_post_dict)
 
 
 if __name__ == "__main__":
--- sunday/classes/jsonablePost.py
+++ sunday/classes/jsonablePost.py
@@ -8,7 +8,7 @@ class JsonablePost(ExtendedPost):
         super().__init__(user_id, post_id, title, body, created_at)
 
     def __repr__(self):
-        return "Class instance successfully created from post #" + str(self.post_id)
+        return self.__dict__
 
     def to_json(self):
         return json.dumps(self.__dict__)
