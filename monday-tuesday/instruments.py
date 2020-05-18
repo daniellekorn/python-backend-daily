2b5d22329f295af80dc70a12db18ec8ee2ce4ce1
Danielle Korn <daniella.korn@gmail.com>
2020-05-18 15:02:46 +0300

    basic structure for user/instrument class constructors

--- monday-tuesday/instruments.py
+++ monday-tuesday/instruments.py
@@ -1,2 +1,8 @@
 class Instrument:
-    pass
+
+    def __init__(self, id_num, owner_id, type):
+        self.id_num = id_num # str
+        self.owner_id = owner_id # user_id str of owner/muscician
+        self.type = type #str: guitar, drums, etc.
+
+
--- monday-tuesday/users.py
+++ monday-tuesday/users.py
@@ -1,2 +1,7 @@
 class User:
-    pass
+
+    def __init__(self, user_id, name, role, instruments):
+        self.user_id = user_id # uuid str
+        self.name = name # str
+        self. role = role # str: musician, manager, etc.
+        self.instruments = instruments # array
